1abf40019a6999292702ef8b82f296a550ef57a9
sanjay <sanjaysanjayr2006@gmail.com>
2026-07-13 19:50:05 +0530

    Fix prompt message for first number input

--- sum.c
+++ sum.c
@@ -3,7 +3,7 @@ int main(){
     int a;
     int b;
     int sum;
-    printf("Enter the first number2 ");
+    printf("Enter the first number: ");
     scanf("%d",&a);
     printf("Enter the second number: ");
     scanf("%d",&b);
