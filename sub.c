b8b719723e5f7249d773f23aadf9fa0a29ff5514
sanjay-0030 <sanjaysanjayr2006@gmail.com>
2026-07-14 19:45:19 +0530

    sub.c

---
+++ sub.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int a,b,sum;
+    printf("Enter number:");
+     scanf("%d",&a);
+      printf("Enter number:");
+     scanf("%d",&b);
+     sum=a-b;
+    printf("%d",sum);
+    return 0;
+
+}
